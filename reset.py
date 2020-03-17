cac5a9fc6a5e1303db49a3dacc7f23a692e21235
clgtruch <truchquarant@gmail.com>
2020-03-17 10:32:30 +0100

    add reset.py

---
+++ reset.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+import sys
+import re
+
+classe = sys.argv[1]
+
+if classe == "62":
+    os.chdir("62ekf")
+
+with open("{}.html".format(classe),"r") as fichier:
+    cont = fichier.read()
+
+cont = re.sub(r"<!--ADD-->[\W,\w]*<!--FIN-->","<!--ADD-->",cont)
+print(cont)
+
+with open("{}.html".format(classe),"w") as fichier:
+    fichier.write(cont)
