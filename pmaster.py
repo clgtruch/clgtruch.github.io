55592411ffb9a6e746b51d7a2cbbd2edc2e08976
clgtruch <truchquarant@gmail.com>
2020-03-18 15:47:50 +0100

    update masterpage

--- pmaster.py
+++ pmaster.py
@@ -26,8 +26,8 @@ clascont = {}
 for classe in clascode:
     with open("{}/{}.html".format(clascode[classe],classe),"r") as fichier:
         clascont[classe] = fichier.read()
-    clascont[classe] = multisplit(clascont[classe],"<title>","</title>","<h1>","</h1>","<!--BEFORE-->","<!--VFIN-->")
-    clascont[classe] = ["<title>"+clascont[classe][1]+"</title>","<h1>"+clascont[classe][3]+"</h1>","<!--BEFORE-->"+clascont[classe][5]+"<!--VFIN-->"]
+    clascont[classe] = multisplit(clascont[classe],"<title>","</title>","<h1>","</h1>","<!--ADD-->","<!--VFIN-->")
+    clascont[classe] = ["<title>"+clascont[classe][1]+"</title>","<h1>"+clascont[classe][3]+"</h1>","<!--ADD-->"+clascont[classe][5]+"<!--VFIN-->"]
     clascont[classe] = mascont.replace("<title>TITRE</title>",clascont[classe][0]).replace("<h1>TITRE</h1>",clascont[classe][1])\
         .replace("<!--CONTENT-->",clascont[classe][2])
     with open("{}/{}.html".format(clascode[classe],classe),"w") as fichier:
