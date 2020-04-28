b2453770ee2f4fd8550287051094ca0d59b90b39
clgtruch <truchquarant@gmail.com>
2020-04-28 21:11:35 +0200

    calcment 6 5

---
+++ calcment/5/calcul.js
@@ -0,0 +1,72 @@
+//Using JSCalcment v2.3 Helluy 2020
+
+var boutonFin = document.getElementById("fin"),
+    pScore = document.getElementById("score"),
+    temps = 0,
+    pTemps = document.getElementById("temps"),
+    jeuEnCours = true,
+    listeCalculs = [
+        new CalculRep("58 + 9"),
+        new CalculRep("300 - 101"),
+        new CalculRep("8 + 8 + 8 + 8 + 8"),
+        new CalculRep("0.3 * 0.9"),
+        new CalculRep("","Produit de deux facteurs égaux à 7","49"),
+        new CalculRep("","4 * ... = 2,4","0.6"),
+        new CalculRep("","Nombre cent fois plus grand que 3 centièmes","3"),
+        new CalculRep("6 * 1.5")
+    ];
+
+boutonFin.addEventListener("click", function (e) {
+    if (jeuEnCours) {
+        var firstError;
+        for (var i = 0; i < listeCalculs.length; i++) {
+            if (listeCalculs[i].areErrors()) {
+                if (typeof firstError == "undefined") {
+                    firstError = i;
+                }
+            }
+        }
+        if (typeof firstError != "undefined") {
+            listeCalculs[firstError].elements.inputs[0].focus()
+        }
+
+    }
+    if (typeof firstError == "undefined") {
+        var listeJF = 0; //nombre de réponses justes
+        for (var i = 0; i < listeCalculs.length; i++) {
+            listeJF += listeCalculs[i].isCorrect() ? 1 : 0;
+        }
+        pScore.textContent = "Votre score est " + Math.round(listeJF / listeCalculs.length * 100) + "%";
+        timerON = false;
+    }
+});
+
+document.addEventListener("keyup", function (e) {
+    if (e.keyCode == 13) {
+        document.activeElement.blur();
+        boutonFin.click();
+    }
+});
+
+setInterval(function () {
+    if (timerON) {
+        temps++;
+        pTemps.textContent = "Votre temps : " + temps + " secondes"
+        // if (temps <= 0) {
+        //     document.activeElement.blur()
+        //     pTemps.textContent = "Le temps est écoulé !";
+        //     pTemps.style.color = "red";
+        //     listeInput = document.getElementsByTagName("input");
+        //     //On empêche la modification des réponses
+        //     for (i = 0; i < listeInput.length; i++) {
+        //         listeInput[i].disabled = true;
+        //     }
+        //     timerON = false;
+        //     if (jeuEnCours) {
+        //         confirm("Le temps est écoulé !");
+        //     }
+        //     jeuEnCours = false;
+        //     boutonFin.click()
+        // }
+    }
+}, 1000);
