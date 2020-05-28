55fdb7a3cbaca551ae79ebd08390388d3eaa38db
clgtruch <truchquarant@gmail.com>
2020-05-28 09:08:30 +0200

    calcment 5 5

---
+++ calcment5/5/calcul.js
@@ -0,0 +1,74 @@
+//Using JSCalcment v2.5 Helluy 2020
+
+var boutonFin = document.getElementById("fin"),
+    pScore = document.getElementById("score"),
+    temps = 0,
+    pTemps = document.getElementById("temps"),
+    jeuEnCours = true,
+    listeCalculs = [
+        new CalculRep("","Quel nombre faut-il ajouter à 6 pour obtenir 0 ?","-6"),
+        new CalculRep("","Quel nombre faut-il ajouter à 3 pour obtenir 2 ?","-1"),
+        new CalculRep("","Quel nombre faut-il ajouter à -9 pour obtenir -2 ?","7"),
+        new CalculRep("","0 + ... = -5","-5"),
+        new CalculRep("","(-4) + .... = -5,7","-1.7"),
+        new CalculRep("","...+ (–9) = (+8)","17"),
+        new CalculRep("","...+(+9)=1","-8"),
+        new CalculRep("","...+(–7)=–2","5"),
+        new CalculRep("","...+(+5)=(–5)","-10"),
+        new CalculRep("","(-3)+...=–7","-4")
+    ]
+
+boutonFin.addEventListener("click", function (e) {
+    if (jeuEnCours) {
+        var firstError;
+        for (var i = 0; i < listeCalculs.length; i++) {
+            if (listeCalculs[i].areErrors()) {
+                if (typeof firstError == "undefined") {
+                    firstError = i;
+                }
+            }
+        }
+        if (typeof firstError != "undefined") {
+            listeCalculs[firstError].elements.inputs[0].focus()
+        }
+
+    }
+    if (typeof firstError == "undefined") {
+        var listeJF = 0; //nombre de réponses justes
+        for (var i = 0; i < listeCalculs.length; i++) {
+            listeJF += listeCalculs[i].isCorrect() ? 1 : 0;
+        }
+        pScore.textContent = "Votre score est " + Math.round(listeJF / listeCalculs.length * 100) + "%";
+        timerON = false;
+    }
+});
+
+document.addEventListener("keyup", function (e) {
+    if (e.keyCode == 13) {
+        document.activeElement.blur();
+        boutonFin.click();
+    }
+});
+
+setInterval(function () {
+    if (timerON) {
+        temps++;
+        pTemps.textContent = "Votre temps : " + temps + " secondes"
+        // if (temps <= 0) {
+        //     document.activeElement.blur()
+        //     pTemps.textContent = "Le temps est écoulé !";
+        //     pTemps.style.color = "red";
+        //     listeInput = document.getElementsByTagName("input");
+        //     //On empêche la modification des réponses
+        //     for (i = 0; i < listeInput.length; i++) {
+        //         listeInput[i].disabled = true;
+        //     }
+        //     timerON = false;
+        //     if (jeuEnCours) {
+        //         confirm("Le temps est écoulé !");
+        //     }
+        //     jeuEnCours = false;
+        //     boutonFin.click()
+        // }
+    }
+}, 1000);
